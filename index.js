59bae44e9b45fd9a6e1f4af71bf77f9a6d07a00e
sh1eldma1den <@pOOsa856100>
2022-09-20 13:45:42 -0400

    lesson two initial push

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 const express = require('express');
-const mongoose = require('mongoose');
+
 const port = process.env.PORT || 3000;
-// require the use of the .env file with username and pw
-require('dotenv').config();
-const app = express();
-// connect to mongodb
-// mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds241658.mlab.com:41658/test_db`,(err)=>{
-
-// if(err) throw err;
-
-// console.log('DB Connected Successfully');
-// })
-
-
 
 app.use('/', require('./routes'))
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`)
 })
+// require the use of the .env file with username and pw
+require('dotenv').config();
+const app = express();
+const MongoClient = require('mongodb');
+
+app.listen(port);
+
+const cors = require('cors');
+
+app.use(cors());
+
+app.get('/contacts', async function(req, res){
+    const uri = process.env.MONGO_URI.replace('dbname', 'cse341');
+
+    const client = new MondoClient(uri);
+
+    const dbo = client.db('cse341');
+
+    const person = await dbo.collection('cse341').findOne();
+
+    res.send(person);
+})
+if(err) throw err;
+
+console.log('DB Connected Successfully');
+
