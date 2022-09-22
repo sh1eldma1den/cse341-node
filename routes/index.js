cbaa227556703d61ae079b6ba4581287e0057511
sh1eldma1den <@pOOsa856100>
2022-09-22 12:01:48 -0400

    connection attempt

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const routes = require('express').Router();
-const userController = require('./controllers/user');
+const userController = require('../controllers/user.js');
 
 routes.get('/', userController.getUser);
 routes.get('/username', userController.getUsername);
