f9d396423edaaa69972c3db114655e435b11219e
sh1eldma1den <@pOOsa856100>
2022-09-23 13:30:05 -0400

    rest client fixes

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,18 +1,24 @@
 const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
 
 const getContacts = async (req, res, next) => {
-  const result = await mongodb.getDb().db('cse341').collection('contacts').findAll();
+  const result = await mongodb.getDb().db().collection('contacts').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
+    res.status(200).json(lists);
   });
 };
 
 const getPerson = async (req, res, next) => {
-  const result = await mongodb.getDb().db('cse341').collection('contacts').find();
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDb
+    .db
+    .collection('contacts')
+    .find({ _id: userId});
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]._id(ObjectId('6329c369d876ca3c2783bea9')));
+    res.status(200).json(lists[0]);
   });
 };
 
