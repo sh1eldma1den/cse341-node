e3c5970756f481b108666ac63caca6a7077310fd
sh1eldma1den <@pOOsa856100>
2022-09-23 14:00:26 -0400

    changes

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongodb = require('./db/connect');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+})
 app.use('/', require('./routes'));
 require('dotenv').config();
 
