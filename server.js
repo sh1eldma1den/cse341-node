4219b3a625ce372ba65596e37f0c7f2ec2f3f549
sh1eldma1den <@pOOsa856100>
2022-09-24 12:58:43 -0400

    swagger interface changes

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.use(bodyParser.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control--Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
 });
 app.use(cors());
